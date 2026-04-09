3113342379768e4f41df3f6ddb0288da39e78196
EdouardSence <edouard.sence@ynov.com>
2026-04-09 12:14:55 +0200

    feat(config): ajout de l'analyse des bundles et mise à jour de la configuration Next.js

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,14 @@
+import bundleAnalyzer from "@next/bundle-analyzer";
 import type { NextConfig } from "next";
 
+const withBundleAnalyzer = bundleAnalyzer({
+  enabled: process.env.ANALYZE === "true",
+});
+
 const nextConfig: NextConfig = {
+  experimental: {
+    optimizePackageImports: ["zod"],
+  },
   images: {
     remotePatterns: [
       {
@@ -15,4 +23,4 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default nextConfig;
+export default withBundleAnalyzer(nextConfig);
